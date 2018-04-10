e31c1f74d092de01cc88251e9c80e9f40344a2b5
Dan K <kuchandw@gmail.com>
2018-04-09 22:10:50 -0700

    Updates
    Completed Homework for Class 1

--- main.py
+++ main.py
@@ -4,7 +4,7 @@ import base64
 from flask import Flask, render_template, request, redirect, url_for, session
 
 from model import Donation 
-
+totaldonation = 0
 app = Flask(__name__)
 
 @app.route('/')
@@ -15,9 +15,13 @@ def home():
 def all():
     donations = Donation.select()
     return render_template('donations.jinja2', donations=donations)
-    
+
+@app.route('/<string:dname>/') 
+def donationsfordonor(dname):
+    donations = Donation.select()
+    return render_template('individualdon.jinja2', dname=dname, donations=donations)
 
 if __name__ == "__main__":
     port = int(os.environ.get("PORT", 6738))
-    app.run(host='0.0.0.0', port=port)
+    app.run(host='0.0.0.0', port=port, debug = True)
 
